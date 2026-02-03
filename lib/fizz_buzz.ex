846b545f7dc757412426674a0836226e7f4dad91
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-03 08:23:28 -0300

    remove comments used in learning processing

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,40 +1,18 @@
 defmodule FizzBuzz do
-  # def build(file_name) do
-  #   {:ok, file} = File.read(file_name)
-  #   file
-  # end
-
-  # ---
-
-  # def build(file_name) do
-  #   case File.read(file_name) do
-  #     {:ok, result} -> result
-  #     {:error, reason} -> reason
-  #   end
-  # end
-
-  # def build(file_name) do
-  #   file = File.read(file_name)
-  #   handle_file_read(file)
-  # end
-
   def build(file_name) do
     file_name
     |> File.read()
     |> handle_file_read()
   end
 
-  # def handle_file_read({:ok, result}) do result end
   defp handle_file_read({:ok, result}) do
     response =
       String.split(result, ",")
-      # |> Enum.map(fn number -> String.to_integer(number) end)
       |> Enum.map(&convert_and_evaluate_numbers/1)
 
     {:ok, response}
   end
 
-  # def handle_file_read({:error, reason}) do reason end
   defp handle_file_read({:error, reason}), do: {:error, "Error reading file: #{reason}"}
 
   defp convert_and_evaluate_numbers(number) do
